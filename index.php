5d038c6670ac1d4682772357d1cfd9a957ccc6c5
seoplay88 <160406737+seoplay88@users.noreply.github.com>
2024-03-06 19:08:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+
+<!doctype html>
+<html amp lang="id">
+  <head>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1">
+    <title>UNIKBET: Situs Judi Slot Hoki Mudah Jackpot Hari Ini</title>
+    <meta name="title" content="UNIKBET: Situs Judi Slot Hoki Mudah Jackpot Hari Ini">
+    <meta name="description" content="UNIKBET adalah situs judi online slot hoki mudah jackpot dengan RTP slot hoki tertinggi hari ini di Indonesia. Mainkan link slot hoki untuk kemenangan jackpot terbesar di Unikbet.">
+    <meta name="keywords" content="unikbet, slot hoki" />
+    <meta name="HandheldFriendly" content="true">
+    <meta name="MobileOptimized" content="width">
+    <meta name="robots" content="index, follow"/>
+    <meta name="page-locale" content="id,en">
+    <meta name="theme-color" content="#000"/>
+    <meta name="categories" content="Unikbet"/>
+    <meta name="language" content="ID">
+    <meta name="rating" content="general">
+    <meta name="copyright" content="Unikbet">
+    <meta name="author" content="Unikbet">
+    <meta name="distribution" content="global">
+    <meta name="publisher" content="Unikbet">
+    <meta name="geo.placename" content="DKI Jakarta"> 
+    <meta name="geo.country" content="ID">
+    <meta name="geo.region" content="ID"/>
+    <meta name="tgn.nation" content="Indonesia">
+    <meta property="og:site_name" content="Unikbet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script async src="https://cdn.ampproject.org/v0.js"></script>  
+    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="img/fav.webp" type="image/x-icon">
+    <link rel="canonical" href="https://www.realvalueproject.com">
+    <link rel="alternate" hreflang="id" href="https://www.realvalueproject.com"/>
+    <link rel="amphtml" href="https://ampunikbet.com/realvalueproject/">
+    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+    <style amp-custom>*{margin:0;padding:0}:focus{outline:0}::-webkit-scrollbar{display:none}body{background-color:#000;background-image:url(img/background.jpeg);background-size:cover;background-position:center;color:var(--white);font-family:system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Helvetica Neue",sans-serif}a,a:after,a:hover,a:visited{text-decoration:none;color:#fff}body{color:#bdc1c6;font-family:"Noto Sans",arial,sans-serif}.atas{display:grid;min-height:100vh}.atasbox{margin:auto;text-align:center}.ataslink{display:inline-grid;margin:.88rem 0}.ataslink a{padding:.5rem 3.8rem;background:#333;margin-bottom:.5rem;border-radius:.38rem;box-shadow:0 -1px #ccb38a88;letter-spacing:1px}.ataslink a.btn1{color:#eee;background-image:linear-gradient(to right,#ff3800,orange);box-shadow:none;font-weight:700}.imghero{box-shadow:inset 0 0 0 8px #888;border-radius:8px}.p{text-align:center;font-size:10px;color:#fff}</style>
+  </head> 
+  <body>
+    <main>
+      <div class="atas">
+        <div class="atasbox">
+          <div>
+            <amp-img style="border-radius: 10px;" height="135" width="230" alt="Unikbet" src="img/slot-gacor.webp"></amp-img>
+          </div>
+          <div>
+            <amp-img style="border-radius: 10px;" height="300" width="230" alt="Slot Hoki" src="img/slot-hoki-unikbet.gif"></amp-img>
+          </div>
+          <div class="ataslink"><a href="https://unikbet.link/masuk" class="btn1">LOGIN</a><a href="https://unikbet.link/masuk" target=_blank rel="noopener noreferrer nofollow">DAFTAR</a><a href="https://unikbet.link/livechat" target=_blank rel="noopener noreferrer nofollow">LIVE CHAT</a></div>
+          <div>
+            <amp-img style="border-radius: 10px;" height="50" width="50" alt="Unikbet" src="img/logo.png"></amp-img>
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
